d774b82e1b6f5d7e87ee2b9afc8240fbced90d61
Francisco <public@francisco.io>
2021-11-25 02:28:12 +0900

    Throw errors properly

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ export function useAsyncEffect(cb, deps = []) {
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
-    Promise.resolve(cb(signal)).then(async res => {
-      // Handle any possible cleanup
-      if (res && typeof res === "function") {
-        if (signal.aborted) res();
-        signal.addEventListener("abort", res);
-      }
-    });
+    Promise.resolve(cb(signal))
+      .then(async res => {
+        // Handle any possible cleanup
+        if (res && typeof res === "function") {
+          if (signal.aborted) res();
+          signal.addEventListener("abort", res);
+        }
+      })
+      .catch(error => {
+        // These errors are not really errors, it's just that JS handles aborting
+        // a promise as an error. Thus, ignore them since they're a normal part
+        // of the expected async workflow
+        if (error.name === "AbortError") {
+          return null;
+        } else {
+          // Real errors, buble it up since the CB is expected to handle the
+          // errors by itself, so this is like a "fatal error" that should've
+          // been handled by the devs
+          throw error;
+        }
+      });
     return () => {
       if (signal.aborted) return;
       controller.abort();
